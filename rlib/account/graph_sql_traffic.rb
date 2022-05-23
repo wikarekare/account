99be6a3151495e825acdbc8342afa392ac15573b
robertburrowes <r.burrowes@auckland.ac.nz>
2022-05-23 17:33:58 +1200

    rubocop cleanup

--- rlib/account/graph_sql_traffic.rb
+++ rlib/account/graph_sql_traffic.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require "mysql"
+require 'mysql'
 require 'pp'
 require 'date'
 require 'time'
@@ -7,14 +7,14 @@ require 'tmpfile'
 include TmpFileMod
 require_relative '../datetime_ext.rb'
 
-#Uber class for common methods
+# Uber class for common methods
 class Graph_Parent
   attr_reader :images
   attr_accessor :debug
-  
+
   def initialize
     @debug = false ######
-    @images = ""
+    @images = ''
   end
 
   def adsl_ip?(address)
@@ -24,16 +24,17 @@ class Graph_Parent
   def ignore?(dest_address)
     dest_ip = dest_address.split('.')
     return ( adsl_ip?(dest_address) ||
-             dest_ip[0] == '10' ||  #Destination is 10.0.0.0/8
-             ( dest_ip[0] == '192' && dest_ip[1] == '168' )   || #Destination is 192.168.0.0/16
-             ( dest_ip[0] == '100' && dest_ip[1] == '64' )   || #Destination is 100.64.0.0/16
-             ( dest_ip[0] == '172' && dest_ip[1].to_i >= 16 && dest_ip[1].to_i < 32 )   || #Destination is 172.16.0.0/12
-             dest_address ==  '255.255.255.255' || #Broadcast
-             ( dest_ip[0] >= "224" && dest_ip[0] <= "239" )   #Destination is multicast
+             dest_ip[0] == '10' ||  # Destination is 10.0.0.0/8
+             ( dest_ip[0] == '192' && dest_ip[1] == '168' ) || # Destination is 192.168.0.0/16
+             ( dest_ip[0] == '100' && dest_ip[1] == '64' ) || # Destination is 100.64.0.0/16
+             ( dest_ip[0] == '172' && dest_ip[1].to_i >= 16 && dest_ip[1].to_i < 32 ) || # Destination is 172.16.0.0/12
+             dest_address == '255.255.255.255' || # Broadcast
+             ( dest_ip[0] >= '224' && dest_ip[0] <= '239' )   # Destination is multicast
            )
   end
 end
 
+# Manifest
 require_relative 'graph_traffic/graph2d.rb'
 require_relative 'graph_traffic/graph3d.rb'
 require_relative 'graph_traffic/graph_host_usage.rb'
