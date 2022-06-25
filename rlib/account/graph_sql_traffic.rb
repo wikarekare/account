568bdf2affb4d127b49decf37289bab9559e06bf
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-25 13:03:42 +1200

    format

--- rlib/account/graph_sql_traffic.rb
+++ rlib/account/graph_sql_traffic.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'wikk_sql'
 require 'pp'
 require 'date'
@@ -37,14 +36,18 @@ end
 # Manifest
 require_relative 'graph_traffic/graph2d.rb'
 require_relative 'graph_traffic/graph3d.rb'
+
 require_relative 'graph_traffic/graph_host_usage.rb'
 require_relative 'graph_traffic/graph_total_usage.rb'
 require_relative 'graph_traffic/graph_connections.rb'
 require_relative 'graph_traffic/graph_ports.rb'
+
 require_relative 'graph_traffic/graph_ntm_port_histogram.rb'
 require_relative 'graph_traffic/graph_ntm_host_histogram.rb'
 require_relative 'graph_traffic/graph_ntm_port_histogram_trim.rb'
 require_relative 'graph_traffic/graph_ntm_host_histogram_trim.rb'
+
 require_relative 'graph_traffic/graph_flow_host_hist_trim.rb'
 require_relative 'graph_traffic/graph_flow_port_histogram_trim.rb'
+
 require_relative 'graph_traffic/graph_internal_hosts.rb'
