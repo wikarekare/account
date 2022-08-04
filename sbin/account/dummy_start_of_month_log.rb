7568dcd75d9780f33da5eb0c93c4a85639ac6d34
robertburrowes <r.burrowes@auckland.ac.nz>
2022-08-05 09:11:48 +1200

    set date to the 1st, not the day run

--- sbin/account/dummy_start_of_month_log.rb
+++ sbin/account/dummy_start_of_month_log.rb
@@ -11,7 +11,7 @@ require_relative "#{RLIB}/wikk_conf.rb"
 WIKK::SQL.connect(@mysql_conf) do |sql|
   query = <<~SQL
     INSERT INTO log_summary (bytes_in , bytes_out , hostname  , log_timestamp )
-      SELECT 0.0,0.0,site_name, '#{Time.now.strftime('%Y-%m-%d')} 00:00:01'
+      SELECT 0.0, 0.0, site_name, Date_format(now(),'%Y-%m-01 00:00:01')
       FROM customer
       WHERE active = 1
       AND site_name LIKE 'wikk%'
