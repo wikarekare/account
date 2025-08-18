ea266c7db3d598e940d15a37e60b2c909e267ec9
robertburrowes <r.burrowes@auckland.ac.nz>
2025-08-19 11:02:40 +1200

    fix rubocop .between test. Should be numeric

--- rlib/account/graph_traffic/graph_parent.rb
+++ rlib/account/graph_traffic/graph_parent.rb
@@ -19,9 +19,9 @@ class Graph_Parent
              dest_ip[0] == '10' ||  # Destination is 10.0.0.0/8
              ( dest_ip[0] == '192' && dest_ip[1] == '168' ) || # Destination is 192.168.0.0/16
              ( dest_ip[0] == '100' && dest_ip[1] == '64' ) || # Destination is 100.64.0.0/16
-             ( dest_ip[0] == '172' && dest_ip[1].to_i >= 16 && dest_ip[1].to_i < 32 ) || # Destination is 172.16.0.0/12
+             ( dest_ip[0] == '172' && dest_ip[1].to_i.between(16, 31) ) || # Destination is 172.16.0.0/12
              dest_address == '255.255.255.255' || # Broadcast
-              dest_ip[0].between('224', '239')    # Destination is multicast
+             dest_ip[0].to_i.between(224, 239)    # Destination is multicast
            )
   end
 end
